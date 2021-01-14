417db630936aa8db281b860266b0dab0e5bae9bf
nemanja-z <nemanja14zivanovic@gmail.com>
2021-01-14 23:02:57 +0100

    Add test

--- issue-tracker/src/components/ManageUsers.test.js
+++ issue-tracker/src/components/ManageUsers.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import MyView from "./MyView";
+import ManageUsers from "./ManageUsers";
 import "mutationobserver-shim";
 import { createMockClient } from 'mock-apollo-client';
 import { ApolloProvider } from "@apollo/client";
@@ -10,32 +10,15 @@ import { BrowserRouter as Router} from "react-router-dom";
 
 test('loads and displays greeting', async () => {
       const mockClient = createMockClient();
-      const history={};
-      const projects=[{
+      const user_projects=[{
         id:"43242fsfsd",
         name:'Who',
         url:'oooopppp',
-        isActive:true,
-        member:[{username:"bla",
-           email:'truc@moo.com',
-           role:'Contractor'}],
-           manager:{username:"pla",
-           email:'truc@moo.com',
-           role:'Manager'}}, 
-        {
-        id:"dasdas44",
+        isActive:true},
+        {id:"dasdas44",
         name:'See',
         url:'ffffoooo',
-        isActive:true,
-        member:[{id:"42342fdsfsdfsd",
-           username:"bla",
-           email:'truc@moo.com',
-           role:'Contractor'}],
-           manager:{id:"asdas42342",
-           username:"bla",
-           email:'truc@moo.com',
-           role:'Manager'}}]; 
-      const username= "Vera";
+        isActive:true}];
       const users=[
           {username:"bla",
            email:'truc@moo.com',
@@ -50,9 +33,9 @@ test('loads and displays greeting', async () => {
             <Router>
               <ApolloProvider client={mockClient}>
                 <ErrorContext.Provider value={{error:null, dispatch:()=>jest.fn()}}>
-                    <MyView history={history} projects={projects} username={username} users={users}/>
+                    <ManageUsers user_projects={user_projects} users={users}/>
                 </ErrorContext.Provider>
               </ApolloProvider>
             </Router>,)
-  expect(screen.getByText('Project')).toHaveTextContent('Project');
+  expect(screen.getByText('Assign users to your project')).toHaveTextContent('Assign users to your project');
 })
